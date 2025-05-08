ff7cd1cb25c458b74ea2551456b4ea3c01238fed
Lav Vadnagara <lav.vadnagara@adit.com>
2025-05-08 12:32:58 +0530

    Add patient exam detail structure and related data

---
+++ DSheet.js
@@ -0,0 +1,53 @@
+PatientId //d
+OrderId //d
+ExamId //d
+ProviderId //d
+Diabetes //d
+IsInvoiced //d
+HasEditExamPermission //d
+TelehealthFeatureAvailable //d
+PerformOnTheFlyCalculationsForExamOrders //d
+IsLifeStyleQuestionEnabled //d
+IsPriceTransparencyEnabled //d
+Providers //d
+ExamCptCodes //d //to ask which one
+FittingFeeCptCodes // d //to ask which one
+ProcedureCptCodes //d //to ask which one
+PqrsCptCodes //d //to ask which one
+ModifierCodes //d //to ask which one
+DiagnosisCodes //d //to ask which one
+DilationReasons //d
+InsuranceEligibilityId //d 
+DiagnosticCodeA //d
+DiagnosticCodeB //d
+DiagnosticCodeC //d
+DiagnosticCodeD //d
+DiagnosticCodeE //d
+DiagnosticCodeF //d
+DiagnosticCodeG //d
+DiagnosticCodeH //d
+DiagnosticCodeI //d
+DiagnosticCodeJ //d
+DiagnosticCodeK //d
+DiagnosticCodeL //d
+MultipleExamCptWarning //d
+SelectedExamCptCode //d
+SelectedFittingFeeCptCode //d
+SelectedProcedureCptCodes //d
+SelectedPqrsCptCodes //d
+Dilation //d 
+Armd //d
+DiabeticRetinopathy //d
+HighRiskPreDiabetes //d
+Glaucoma //d
+HighCholesterol //d
+Hypertension //d
+Arcus //d
+AbnormalPupil //d
+Cataract //d
+Pcp //d
+EmployeeId //d
+IsEstimated 
+PrimaryInsurance
+SecondaryInsurance
+ItemCharges
